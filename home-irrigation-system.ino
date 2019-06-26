b5fdff6ae6e5b413648ed2a7464d5629184234d8
Gummiberra <Simon.larsson92@gmail.com>
2019-06-26 22:23:18 +0200

    Added secrets file for configuration

--- home-irrigation-system.ino
+++ home-irrigation-system.ino
@@ -1,50 +1,163 @@
-/*
- * TODO
- * Bug when powerloss wont start until d3 and d4 are unplugged during boot
- * Add wifi
- * Add mqtt
- */
-
+#include "secrets.h"
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <WiFiClient.h>
+#include <WiFiClientSecure.h>
+#include <WiFiServer.h>
+#include <WiFiServerSecure.h>
+#include <WiFiUdp.h>
+#include <Keypad.h>
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+#include <PubSubClient.h>
 
 //Analog inputs
 int sensor_pin = A0;
 
+//Digital outputs
 int solenoid_1 = D3;
 int water_pump = D4;
 int light_sensor_vcc = D5;
-int humidity_sensor_vcc = D6;
-int humidity_sensor2_vcc = D7;
+int moisture_sensor_vcc = D6;
+int moisture_sensor2_vcc = D7;
 
+//WIFI
+const char ssid[] = WIFI_SSID;        // SSID of wifi network
+const char password[] = WIFI_PASSWORD;   // Password of wifi netowrk
+WiFiClient espClient;
+
+//MQTT
+const char mqtt_server[] = MQTT_SERVER;                   // MQTT server ip
+const int mqtt_port = 1883;                      // MQTT server port   default: 1883
+const char mqtt_user[] = MQTT_USER;                     // MQTT username
+const char mqtt_password[] = MQTT_PASSWORD;                 // MQTT user password
+const char device_id[] = DEVICE_ID;                     // MQTT device name
+PubSubClient client(espClient);
+char msg[64];
+char topic[32];
+
+//Light sensor
+unsigned long previousMillis = 0;        // will store last time light sensor was updated
+const long interval = 300000;  
 
 void setup() {
-  // Init the pins
-  pinMode(humidity_sensor_vcc, OUTPUT);
-  digitalWrite(humidity_sensor_vcc, LOW);
-  pinMode(humidity_sensor2_vcc, OUTPUT);
-  digitalWrite(humidity_sensor2_vcc, LOW);
+  // Initiate the pins
+  pinMode(moisture_sensor_vcc, OUTPUT);
+  digitalWrite(moisture_sensor_vcc, LOW);
+  pinMode(moisture_sensor2_vcc, OUTPUT);
+  digitalWrite(moisture_sensor2_vcc, LOW);
   pinMode(light_sensor_vcc, OUTPUT);
   digitalWrite(light_sensor_vcc, LOW);
   pinMode(solenoid_1, OUTPUT);
   digitalWrite(solenoid_1, LOW);
   pinMode(water_pump, OUTPUT);
   digitalWrite(water_pump, LOW);
-  
+
+  //Initiate WIFI
   Serial.begin(9600);
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.hostname("Garden Irrigation");
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  //Initiate MQTT
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+  while (!client.connected()) 
+  {
+    Serial.println("Connecting to MQTT...");
+    if (client.connect("ESP8266Client", mqtt_user, mqtt_password )) 
+    {
+      Serial.println("connected");
+      client.subscribe("home/garden_irrigation");  
+    } 
+    else 
+    {
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+    }
+  }
 }
 
-int read_humidity_sensor() {
-  digitalWrite(humidity_sensor_vcc, HIGH);
+void callback(char* topic, byte* payload, unsigned int length) 
+{
+  memcpy(msg, payload, length);
+  msg[length] = '\0';
+ 
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+  Serial.print("Message:");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+  Serial.println("-----------------------");
+
+  if(strcmp(msg,"check_moisture") == 0) 
+  {
+    Serial.println("Reading moisture sensor 1");
+    int sensor_1 = read_moisture_sensor1();
+    snprintf(topic, 32, "home/garden_moisture_sensor_1");
+    snprintf(msg, 64, sensor_1, device_id);
+    client.publish(topic,msg); 
+    delay(1000);
+    Serial.println("Reading moisture sensor 2");
+    int sensor_2 = read_moisture_sensor2(); 
+    snprintf(topic, 32, "home/garden_moisture_sensor_2");
+    snprintf(msg, 64, sensor_2, device_id);
+    client.publish(topic,msg); 
+    
+  }
+  else if(strcmp(msg,"water_zone_1") == 0)
+  {
+    Serial.println("Opening Solenoid");
+    open_solenoid();
+    delay(500);
+    Serial.println("Starting waterpump");
+    snprintf(topic, 32, "home/garden_water_zone_1");
+    snprintf(msg, 64, "Watering", device_id);
+    client.publish(topic,msg);
+    start_water_pump();
+    delay(30000); //Water for 30seconds
+    Serial.println("Stopping waterpump");
+    stop_water_pump();
+    Serial.println("Closing solenoid");
+    close_solenoid();
+    snprintf(topic, 32, "home/garden_water_zone_1");
+    snprintf(msg, 64, "Done", device_id);
+    client.publish(topic,msg); 
+  }
+
+  
+ 
+}
+
+int read_moisture_sensor() {
+  digitalWrite(moisture_sensor_vcc, HIGH);
   delay(500);
   int value = analogRead(sensor_pin);
-  digitalWrite(humidity_sensor_vcc, LOW);
+  digitalWrite(moisture_sensor_vcc, LOW);
   return 1023 - value;
 }
 
-int read_humidity_sensor2() {
-  digitalWrite(humidity_sensor2_vcc, HIGH);
+int read_moisture_sensor2() {
+  digitalWrite(moisture_sensor2_vcc, HIGH);
   delay(500);
   int value = analogRead(sensor_pin);
-  digitalWrite(humidity_sensor2_vcc, LOW);
+  digitalWrite(moisture_sensor2_vcc, LOW);
   return 1023 - value;
 }
 
@@ -73,26 +186,17 @@ void stop_water_pump() {
 }
 
 void loop() {
-  Serial.print("Humidity Sensor 1 Level (0-1023): ");
-  Serial.println(read_humidity_sensor()); 
-  delay(5000);
-  Serial.print("Humidity Sensor 2 Level (0-1023): ");
-  Serial.println(read_humidity_sensor2()); 
-  delay(5000);
-  Serial.print("Light Sensor Level (0-1023): ");
-  Serial.println(read_light_sensor()); 
-  delay(5000);
-  Serial.println("Open solenoid");
-  open_solenoid(); 
-  delay(200);
-  Serial.println("Starting waterpump");
-  start_water_pump(); 
-  delay(5000);
-  Serial.println("Stoping waterpump");
-  stop_water_pump(); 
-  delay(200);
-  Serial.println("Close solenoid");
-  close_solenoid(); 
-  delay(5000);
-  
+  client.loop();
+
+  //Only update light sensor if the interval has passed.
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    // save the last time you updated sensor
+    previousMillis = currentMillis;
+    Serial.println("Reading light sensor");
+    int light = read_light_sensor(); 
+    snprintf(topic, 32, "home/garden_light_sensor");
+    snprintf(msg, 64, light, device_id);
+    client.publish(topic,msg); 
+  }   
 }
